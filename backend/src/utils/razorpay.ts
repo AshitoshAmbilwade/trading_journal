6feb7a1b331f087658ea4de8ea2631c9098288c2
AshitoshAmbilwade <ashitoshambilwade1289@gmail.com>
2025-11-29 15:45:39 +0530

    razorpay ts changes

--- backend/src/utils/razorpay.ts
+++ backend/src/utils/razorpay.ts
@@ -1,5 +1,7 @@
 import Razorpay from "razorpay";
+import dotenv from "dotenv";
 
+dotenv.config();
 export const razorpay = new Razorpay({
   key_id: process.env.RZP_KEY_ID,
   key_secret: process.env.RZP_KEY_SECRET,
