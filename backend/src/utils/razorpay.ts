cd3d61dc9e1e5fc6722d6dfba1eb3bae4e9f8bd9
AshitoshAmbilwade <ashitoshambilwade1289@gmail.com>
2025-11-29 15:41:54 +0530

    razorpay integration

--- backend/src/utils/razorpay.ts
+++ backend/src/utils/razorpay.ts
@@ -1,7 +1,6 @@
-// src/utils/razorpay.ts
 import Razorpay from "razorpay";
 
 export const razorpay = new Razorpay({
-  key_id: process.env.RZP_KEY_ID!,
-  key_secret: process.env.RZP_KEY_SECRET!,
+  key_id: process.env.RZP_KEY_ID,
+  key_secret: process.env.RZP_KEY_SECRET,
 });
